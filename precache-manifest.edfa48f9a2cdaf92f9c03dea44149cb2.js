3ed267bbeef5fd01aa988f689f2d4102ecdfa717
david-ziffity <david.francis@ziffity.com>
2020-09-23 22:28:39 +0530

    Updates

---
+++ precache-manifest.edfa48f9a2cdaf92f9c03dea44149cb2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba5f486aed06681a1da8d79055673169",
+    "url": "/youtube-search/index.html"
+  },
+  {
+    "revision": "f35f660d614840fadf3d",
+    "url": "/youtube-search/static/css/main.1f5ac28e.chunk.css"
+  },
+  {
+    "revision": "0464f8fe30748517b05c",
+    "url": "/youtube-search/static/js/2.9a6db237.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/youtube-search/static/js/2.9a6db237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f35f660d614840fadf3d",
+    "url": "/youtube-search/static/js/main.ac0e37f5.chunk.js"
+  },
+  {
+    "revision": "8565e40cf071b3d6d1ec",
+    "url": "/youtube-search/static/js/runtime-main.6f6815d1.js"
+  }
+]);
